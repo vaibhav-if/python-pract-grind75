d0f967ccbe5f454a74ba0268332ba9931d641515
Vaibhav Agrawal <vaibhavagr711@gmail.com>
2025-03-08 17:27:29 +0530

    Lowest Common Ancestor of a Binary Tree

---
+++ lca-binarytree.py
@@ -0,0 +1,37 @@
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        if root == None or root == p or root == q:
+            return root
+        
+        left = self.lowestCommonAncestor(root.left, p, q)
+        right = self.lowestCommonAncestor(root.right, p, q)
+
+        if left and right:
+            return root
+        
+        return left if left else right
+    
+sol = Solution()
+
+# tree def
+root = TreeNode(3)
+root.left = TreeNode(5)
+root.right = TreeNode(1)
+root.left.left = TreeNode(6)
+root.left.right = TreeNode(2)
+root.right.left = TreeNode(0)
+root.right.right = TreeNode(8)
+root.left.right.left = TreeNode(7)
+root.left.right.right = TreeNode(4)
+
+p = root.left  # 5
+q = root.left.right.right  # 4
+
+print(sol.lowestCommonAncestor(root, p, q).val)
